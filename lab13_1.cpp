85bc9e243d6dc94fb08a70733d71181a6dff0352
ChonlananT <ciwerxil@gmail.com>
2021-12-24 15:55:28 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int N){
+    if(N == 0)  return 0;
+    else if(N == 1) return 1;
+    else{
+        int a = fibonacci(N-1) + fibonacci(N-2);
+        return a;
+    }
 }
